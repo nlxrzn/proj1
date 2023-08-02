ed7b55948ac8b312c151099cd86c885598f715dc
nlxrzn <S101262575@connect.np.edu.sg>
2023-08-03 00:07:18 +0800

    find day with the highest amount

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -14,5 +14,29 @@ with fp.open(mode ="r", encoding = "UTF-8", newline = "") as file:
 
     #append cash on hand into cashrecords lis
     for row in reader:
-        cashRecords.append([row[0],row[2]])
+        cashRecords.append([row[0],row[1]])
+    
+    def find_day_with_highest_increment(cashRecords):
+     """Finds the day and amount of the highest increment in Cash-on-Hand.
 
+    Args:
+    data: A list of lists, where each inner list contains the day and amount of Cash-on-Hand for a given day.
+
+  Returns:
+    A tuple of the day and amount of the highest increment, or None if there are no increments.
+  """
+    highest_increment = 0
+    day_with_highest_increment = None
+    for i in range(1, len(cashRecords)):
+        current_value = int(cashRecords[i][1])
+        previous_value = int(cashRecords[i - 1][1])
+
+        if current_value - previous_value > highest_increment:
+         highest_increment = current_value - previous_value
+         day_with_highest_increment = cashRecords[i][0]
+        # return day_with_highest_increment, highest_increment
+
+    if day_with_highest_increment is None:
+        print("There are no increments in Cash-on-Hand")
+    else:
+     print("The day with the highest increment is", day_with_highest_increment, "with an increment of", highest_increment)
