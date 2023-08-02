12530e1058e8d3e5de3f3ed8c9e10ab7801a559d
darisng <S10262525@connect.np.edu.sg>
2023-08-03 01:39:39 +0800

    minor edits to cash on hand

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -42,3 +42,5 @@ with fp.open(mode ="r", encoding = "UTF-8", newline = "") as file:
     else:
      print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
      print(f"[HIGHEST CASH SURPLUS] DAY:{day_with_highest_increment}, AMOUNT: USD{cash_on_hand_on_day_with_highest_increment}")
+
+    
